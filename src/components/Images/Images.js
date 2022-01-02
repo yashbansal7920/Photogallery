29f4e127e8a51646569f9dcf7c91ba979c3a4d83
Yash <yashbansal7920@gmail.com>
2022-01-02 10:49:50 +0530

    makes a gallery responsive

--- src/components/Images/Images.js
+++ src/components/Images/Images.js
@@ -1,11 +1,13 @@
 import React from "react";
+import "./Images.css";
 
 const Images = ({ images }) => {
   console.log(images);
   return (
-    <div>
+    <div className="gallery">
       {images.map((i) => (
         <img
+          className="pics"
           key={i.id}
           src={`https:farm${i.farm}.staticflickr.com/${i.server}/${i.id}_${i.secret}.jpg`}
           alt={i.title}
--- src/hooks/useFetchImages.js
+++ src/hooks/useFetchImages.js
@@ -8,21 +8,32 @@ const useFetchImages = (query) => {
   console.log(query);
 
   useEffect(() => {
+    const source = axios.CancelToken.source();
+
     const fetchImages = async () => {
       setLoading(true);
+
       try {
-        const { data } =
-          await axios.get(`https://www.flickr.com/services/rest/?method=flickr.photos.search&api_key=e52a287d86469bf01ea901dfd92cf8a5&te
-      xt=${query}&media=photos&per_page=15&page=1&format=json&nojsoncallback=1`);
+        const { data } = await axios.get(
+          `https://www.flickr.com/services/rest/?method=flickr.photos.search&api_key=e52a287d86469bf01ea901dfd92cf8a5&te
+      xt=${query}&media=photos&per_page=15&page=1&format=json&nojsoncallback=1`,
+          {
+            cancelToken: source.token,
+          }
+        );
 
         setImages((prev) => [...new Set([...prev, ...data.photos.photo])]);
       } catch (error) {
+        if (axios.isCancel(error)) return;
         console.log(error);
       } finally {
         setLoading(false);
       }
     };
+
     fetchImages();
+
+    return () => source.cancel();
   }, [query]);
 
   return { loading, images, setImages };
